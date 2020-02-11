0fdb9af75e0d7a130dabc31d83d6b0183bab3d42
NickXitco <nxitco1@jhu.edu>
2020-02-11 01:18:14 -0500

    Progress towards O(nlogn) algorithm

--- java/src/main/java/ZZero.java
+++ java/src/main/java/ZZero.java
@@ -24,50 +24,16 @@ public class ZZero {
 
     private static HashMap<Artist, ConnectedComponent> completeZ0(LinkedList<Artist> artists) {
         HashMap<Artist, ConnectedComponent> artistsInMap = new HashMap<>();
-        HashMap<Artist, LinkedList<ConnectedComponent>> guestList = new HashMap<>();
-        HashSet<ConnectedComponent> connectedComponents = new HashSet<>();
+        HashMap<Artist, LinkedList<Artist>> guestList = new HashMap<>();
+        LinkedList<ConnectedComponent> connectedComponents = new LinkedList<>();
 
         int noArtists = artists.size();
-
         int i = 0;
         for (Artist artist : artists) {//artists.size() && (i < 500 || !(connectedComponents.size() == 1 && artistsInMap.size() > 1))
             ConnectedComponent cc = new ConnectedComponent(artist);
-            artistsInMap.put(artist, cc);
-            LinkedList<ConnectedComponent> invitations = guestList.get(artist);
-            guestList.remove(artist);
-            connectedComponents.add(cc);
-
-            if (invitations != null) {
-                for (ConnectedComponent k : invitations) {
-                    ConnectedComponent topK = bubbleUp(k);
-                    if (topK != cc) {
-                        cc.members.merge(topK.members);
-                        topK.dead = true;
-                        topK.beneficiary = cc;
-                        connectedComponents.remove(topK);
-                    }
-                }
-            }
-
             for (Artist r : artist.relatedArtists) {
-                ConnectedComponent rCC = artistsInMap.get(r);
-                if (rCC != null) {
-                    ConnectedComponent topRCC = bubbleUp(rCC);
-                    if (topRCC != cc) {
-                        artistsInMap.replace(r, cc);
-                        cc.members.merge(topRCC.members);
-                        topRCC.dead = true;
-                        topRCC.beneficiary = cc;
-                        connectedComponents.remove(topRCC);
-                    }
-                } else {
-                    if (guestList.containsKey(r)) {
-                        guestList.get(r).add(cc);
-                    } else {
-                        LinkedList<ConnectedComponent> l = new LinkedList<>();
-                        l.add(cc);
-                        guestList.put(r, l);
-                    }
+                if (artistsInMap.containsKey(r)) {
+                    artistsInMap.replace(r, cc);
                 }
             }
 
@@ -77,26 +43,11 @@ public class ZZero {
             }
         }
 
-        ConnectedComponent largest = connectedComponents.iterator().next();
-        for (ConnectedComponent cc : connectedComponents) {
-            cc.cementedList = cc.members.getList();
-            if (cc.cementedList.size() > largest.cementedList.size()) {
-                largest = cc;
-            }
-        }
-
 
         //TODO find the minimum spanning tree
         //TODO count the leaves in the tree. If there are less than (artistsInMap.size() - 500) leaves, then add more artists, maybe another 500? idk man lol
         //TODO sort those leaves by followers and remove them from the map in ascending follower order until we have 500 artists that are connected :) yay
         return artistsInMap;
-    }
-
-    private static ConnectedComponent bubbleUp(ConnectedComponent k) {
-        while (k.beneficiary != null) {
-            k = k.beneficiary;
-        }
-        return k;
     }
 
     private static LinkedList<Artist> getArtists(BufferedReader bfDatabase, BufferedReader bfArtists) throws IOException {
@@ -155,8 +106,6 @@ public class ZZero {
         int popularity;
         LinkedList<String> genres;
         LinkedList<Artist> relatedArtists;
-        Artist next;
-        Artist prev;
 
         Artist(String name, String id, int followers, int popularity, LinkedList<String> genres) {
             this.name = name;
@@ -183,47 +132,11 @@ public class ZZero {
 
 
     private static class ConnectedComponent {
-        MembersList members;
-        ConnectedComponent beneficiary; //The cc that this component gives its members to once it's been merged. This is done to massively speed up computation and keep the algorithm running in linear time.
-        boolean dead = false;
-        LinkedList<Artist> cementedList;
+        LinkedList<Artist> members;
 
         ConnectedComponent(Artist head) {
-            this.members = new MembersList(head);
-        }
-    }
-
-    private static class MembersList {
-        Artist head;
-        Artist tail;
-
-        MembersList(Artist a) {
-            this.head = a;
-            this.tail = a;
-        }
-
-        LinkedList<Artist> getList() {
-            LinkedList<Artist> list = new LinkedList<>();
-            Artist current = this.head;
-            while (current != null) {
-                list.add(current);
-                current = current.next;
-            }
-            return list;
-        }
-
-        void add(Artist a) {
-            this.tail.next = a;
-            a.prev = this.tail;
-            this.tail = a;
-        }
-
-        void merge(MembersList toBeMerged) {
-            toBeMerged.head.prev = this.tail;
-            this.tail.next = toBeMerged.head;
-            this.tail = toBeMerged.tail;
-            toBeMerged.head = null;
-            toBeMerged.tail = null;
+            this.members = new LinkedList<>();
+            this.members.add(head);
         }
     }
 
